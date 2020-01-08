41b14713e46a86e059a0962342a1854c500b43fc
samir <51944910+samir2397@users.noreply.github.com>
2020-01-08 19:41:20 +0400

    details

--- detail/js/app.js
+++ detail/js/app.js
@@ -53,3 +53,17 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+$(function(){
+  $(".remove-bar").on("click",function(){
+    $(this).addClass("d-none")
+    $(".rmv-ico").removeClass("d-none")
+    $(".menu").removeClass("menuActive")
+  })
+  $(".rmv-ico").on("click",function(){
+    $(this).addClass("d-none")
+    $(".menu").addClass("menuActive")
+
+    $(".remove-bar").removeClass("d-none")
+  })
+})
